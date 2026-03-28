8f77b71c205aa4b27bcb394a7f6cddd431289b41
Mathis Louron <chevrethis@MacBook-Air-de-Mathis.local>
2026-03-28 15:46:40 +0100

    fix:port issue

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ export default defineConfig({
   plugins: [react()],
   preview: {
     host: '0.0.0.0',
+    port: parseInt(process.env.PORT) || 8080,
     allowedHosts: [
       'localhost',
       '127.0.0.1',
